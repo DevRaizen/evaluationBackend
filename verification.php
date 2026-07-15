72dd005928769285762e898957b6a48522a35ede
devRaizen <shawnbulos03@gmail.com>
2026-07-15 13:28:30 +0800

    try

--- verification.php
+++ verification.php
@@ -29,7 +29,7 @@ try {
     $mail->Username = 'sharmainepagador@gmail.com';
     $mail->Password = 'dxch fixs btve myot';
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mail->Port = 465;
+    $mail->Port = 586;
 
     $mail->Timeout = 15;
 $mail->SMTPDebug = 2;
